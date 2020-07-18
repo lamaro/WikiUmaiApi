98052f0bf8695b9c9d84206013c0672eeeee3c6f
Leandro Amaro Garcia <>
2020-07-18 19:36:53 -0300

    datos db 3

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
           client: "mysql",
           host: env("DATABASE_HOST", "127.0.0.1"),
           port: env.int("DATABASE_PORT", 3306),
-          database: env("wikiumai_strapi_db", "strapi"),
-          username: env("wikiumai_umai", ""),
-          password: env("{v*NnVt7H-Tj", ""),
+          database: env("wikiumai_strapi_db", "wikiumai_strapi_db"),
+          username: env("wikiumai_umai", "wikiumai_umai"),
+          password: env("{v*NnVt7H-Tj", "{v*NnVt7H-Tj"),
         },
         options: {
           ssl: false,
