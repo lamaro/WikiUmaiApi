0d8e176ea3560fcca0f6db12563a5c3f2422fd90
Leandro Amaro Garcia <>
2020-07-18 18:30:27 -0300

    datos db

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "mysql",
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("wikiumai_strapi_db", "strapi"),
+          username: env("wikiumai_umai", ""),
+          password: env("{v*NnVt7H-Tj", ""),
+        },
+        options: {
+          ssl: false,
+        },
       },
     },
-  },
-});
+  });
