a1b4b17b007a7e64b3d06cc74e8884be2acc65c4
Leandro Amaro Garcia <>
2020-08-09 15:58:23 -0300

    agregado slugs a collections y models

--- api/hitos/models/hitos.js
+++ api/hitos/models/hitos.js
@@ -1,8 +1,18 @@
-'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            console.log('before creating', data)
+            if (data.titulo) {
+                data.slug = slugify(data.titulo,{lower:true});
+            }
+        },
+        async beforeUpdate(params, data) {
+            console.log('before updating', data)
+            if (data.titulo) {
+               data.slug = slugify(data.titulo,{lower:true});
+            }
+        },
+    },
+};
