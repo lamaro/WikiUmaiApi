860c9ee28887a15e807935d00241c6d567c04cc2
Leandro Amaro Garcia <>
2020-09-18 10:28:01 -0300

    moving server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-    apps : [{
-      name: "strapi",
-      script: "./server.js",
-      env: {
-        NODE_ENV: "development",
+    apps: [
+      {
+        name: 'strapi',
+        cwd: '/var/www/wwwroot/nodejs.revisionalpha.com',
+        script: './server.js',
+        env: {
+          NODE_ENV: 'production',
+          DATABASE_HOST: 'localhost', // database endpoint
+          DATABASE_PORT: '3306',
+          DATABASE_NAME: 'wiki_api', // DB name
+          DATABASE_USERNAME: 'wiki_api', // your username for psql
+          DATABASE_PASSWORD: '4YCkbXDcGPNLHzLG', // your password for psql
+        },
       },
-      env_production: {
-        NODE_ENV: "production",
-      }
-    }]
-  }
+    ],
+  };
