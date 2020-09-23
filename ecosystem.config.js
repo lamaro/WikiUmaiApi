91a2cc5a04e3c4850f9e0979b11ac9d042e9d1d4
Leandro Amaro Garcia <>
2020-09-23 20:05:16 -0300

    args start 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
           DATABASE_PORT: '3306',
           DATABASE_NAME: 'wiki_api', // DB name
           DATABASE_USERNAME: 'wiki_api', // your username for psql
-          DATABASE_PASSWORD: '4YCkbXDcGPNLHzLG', // your password for psql
+          DATABASE_PASSWORD: '{v*NnVt7H-Tj', // your password for psql
         },
       },
     ],
