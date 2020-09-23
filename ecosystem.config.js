c9867346a2ddf7e4c8a45bba34342269a36f9e09
Leandro Amaro Garcia <>
2020-09-23 20:06:57 -0300

    args start 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
           DATABASE_HOST: 'localhost', // database endpoint
           DATABASE_PORT: '3306',
           DATABASE_NAME: 'wiki_api', // DB name
-          DATABASE_USERNAME: 'wiki_api', // your username for psql
+          DATABASE_USERNAME: 'wikiumai_umai', // your username for psql
           DATABASE_PASSWORD: '{v*NnVt7H-Tj', // your password for psql
         },
       },
