6d9a94fb463c8ee0b74df603da258b96c66aa98d
Leandro Amaro Garcia <>
2020-09-25 10:15:58 -0300

    change user

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
           NODE_ENV: 'production',
           DATABASE_HOST: 'localhost', // database endpoint
           DATABASE_PORT: '3306',
-          DATABASE_NAME: 'wiki_api', // DB name
+          DATABASE_NAME: 'wikiumai_api', // DB name
           DATABASE_USERNAME: 'wikiumai_umai', // your username for psql
           DATABASE_PASSWORD: '{v*NnVt7H-Tj', // your password for psql
         },
