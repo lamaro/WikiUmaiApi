e0d413c325fb4de8cdd5e8dc941d5d1dec3b6913
Leandro Amaro Garcia <>
2020-11-06 10:35:59 -0300

    custom planetas

--- api/planetas/controllers/planetas.js
+++ api/planetas/controllers/planetas.js
@@ -1,8 +1,26 @@
-'use strict';
+const _ = require("lodash");
+module.exports = {
+    async index(ctx) {
+        const planetas = await strapi
+            .query("planetas")
+            .find({ _sort: "updated_at:desc" });
+        const finalArray = planetas.map((planeta) => _.omit(planeta, ['historias', 'personajes', 'hitos', 'objetos']))
+        return finalArray
+    },
+    async planetaBySlug(ctx) {
+        const { slug } = ctx.params
+        const planetas = await strapi
+            .query("planetas")
+            .find({ _where: [{ slug }] });
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+        const finalArrayHistorias = planetas[0].historias.map((historia) => _.omit(historia, ['contenido', 'sinopsis', 'planeta']))
+        const finalArrayPersonajes = planetas[0].personajes.map((personaje) => _.omit(personaje,
+            ['sinopsis', 'raza', 'introduccion', 'genero', 'edad', 'altura', 'profesion', 'habilidades', 'caracteristicas', 'galeria','objeto']
+        ))
+        const finalArrayObjetos = planetas[0].objetos.map((objeto) => _.omit(objeto, ['planeta', 'tipo', 'origen', 'contexto_uso', 'modo_uso', 'impacto_social', 'descripcion']))
 
-module.exports = {};
+        const finalArray = [{ ...planetas[0], historias: finalArrayHistorias, personajes: finalArrayPersonajes, objetos: finalArrayObjetos }]
+
+        return finalArray
+    }
+};
